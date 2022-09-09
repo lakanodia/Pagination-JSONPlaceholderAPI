8150020c6309baba51a6c70bad66853549b60511
Laka Nodia <laka.nodia@gmail.com>
2022-09-10 00:51:33 +0400

    Add open post overlay function

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-let divElement = document.getElementById("p-tag");
-let content = document.getElementById("div-wrapper");
+// let content = document.getElementById("div-wrapper");
 let peginationItem = document.getElementById("nav-number");
 let currentNumPost = document.querySelectorAll(".num-item");
+let postOverlay = document.querySelector('.post-overlay');
+
 
 let pageIndex = 1;
 let itemPerPage = 10;
 let isFetching = 0;
 let postList = [];
-let totalNum = 100;
+let totalNum = 200;
 
 
 
@@ -23,7 +24,7 @@ function getPosts() {
   )
     .then((response) => response.json())
     .then((jsonData) => {
-      // sleepTimer(1);
+      sleepTimer(1);
       postList = jsonData;
       drawAllPosts();
       loadPagination();
@@ -32,11 +33,11 @@ function getPosts() {
 
 function sleepTimer(sec) {
   let endTime = new Date().getTime() + sec*1000;
-  while (new Date().getTime() <= endTime) {
-    
+  while (new Date().getTime() <= endTime) { 
   }
   console.log('Time Out');
 }
+
 
 function pageRowChangeEventHandler() {
   itemPerPage = document.getElementById("page-row-number").value;
@@ -44,17 +45,51 @@ function pageRowChangeEventHandler() {
   getPosts();
 }
 
+
+
 function drawSinglePost(item) {
   return `
-    <div class="card m-3 hover" style="width: 18rem;">
+    <div class="card m-3 hover" style="width: 18rem;"  data-id="${item.id}">
       <img src="${item.url}" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">${item.id}</h5>
         <p class="card-text">${item.title}</p>
-        <a href="#" class="btn btn-primary">View</a>
+        <a href="#" class="btn btn-primary" onclick="openPostCard(this)"" >View</a>
       </div>
     </div>
   `
+}
+
+
+function postCardInfo(item){
+  return `
+    <button class="btn btn-danger" onclick ="closeOverlay()"> Close </button>
+    <div class="card m-3 hover p-5" style="width: 40rem;">
+    
+      <div class="d-flex">
+        <img class="w-50 me-1" src="${item.url}" class="card-img-top" alt="...">
+        <img class="w-50" src="${item.thumbnailUrl}" class="card-img-top" alt="...">
+      </div>
+      <div class="card-body">
+        <h2 class="card-text">${item.title}</h2>
+      </div>
+    </div>`
+ }
+
+function closeOverlay(){
+  postOverlay.classList.remove('active-ovelay');
+}
+
+// // this function opens the specific post that I select
+function openPostCard(item){
+  postOverlay.classList.add('active-ovelay');
+  let id = (item.parentElement.parentElement).getAttribute('data-id');
+  console.log(id);
+  let exactData = (postList[id-1]);
+  console.log(postList[id-1]);
+  let content ="";
+  content+= postCardInfo(exactData);
+  postOverlay.innerHTML = content;
 }
 
 function drawAllPosts() {
@@ -85,6 +120,6 @@ function loadPagination() {
   for (let i = 0; i < totalNum/itemPerPage; i++) {
     content += paginateNum (i+1);
   }
-peginationItem.innerHTML = content;
+  peginationItem.innerHTML = content;
 }
 
